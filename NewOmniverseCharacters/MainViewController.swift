59a7b9c1b1f49d59bf9e5e7544e1266e8e651b19
bronexus <dymkatrakee@gmail.com>
2021-10-12 00:22:29 +0300

    Added non working page buttons

--- NewOmniverseCharacters/MainViewController.swift
+++ NewOmniverseCharacters/MainViewController.swift
@@ -12,32 +12,25 @@ class MainViewController: UIViewController, UITableViewDataSource {
     @IBOutlet var charactersTableView: UITableView!
     
     var charactersList = [SingleCharacter]()
+    var nextPage = String()
+    var prevPage = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
         
-        updateCharactersListByPageURL(urlString: "https://rickandmortyapi.com/api/character")
+        // Fetch to charactersList variable Characters if first page of Rick and Morty API
+        downloadCharactersListByPageURL(urlString: "https://rickandmortyapi.com/api/character")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
     // MARK: - UITableView Functions
     
+    // Define number of rows in Characters Table View
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return charactersList.count
     }
     
+    // Define cell/s for Characters Table View
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = tableView.dequeueReusableCell(withIdentifier: "characterCell") as? CharacterTableViewCell else { return UITableViewCell() }
         
@@ -61,27 +54,38 @@ class MainViewController: UIViewController, UITableViewDataSource {
     }
     
     
+    
+    
+    
+    
     // MARK: - Download Functions
     
-    func updateCharactersListByPageURL(urlString: String) {
+    // Fetch Characters to charactersList variable
+    func downloadCharactersListByPageURL(urlString: String) {
         let downloadURL = URL(string: urlString)
         guard let url = downloadURL else { return }
         URLSession.shared.dataTask(with: url) { data, urlResponse, error in
             guard let data = data, urlResponse != nil, error == nil else {
-                print("Character download Error")
+                print("Character download Error.")
                 return
             }
-            print("Character download Succes")
+            print("Character download Succes.")
             do {
                 let decoder = JSONDecoder()
                 let characterResponse = try decoder.decode(CharacterPageResponse.self, from: data)
-                print("\(characterResponse.results[0].name) is online")
+                print("Loaded page \(urlString) of 34.")
+                print("\(characterResponse.results[0].name) is online.")
+                guard let nextPage = characterResponse.info.next else { return }
+                self.nextPage = nextPage
+                guard let prevPage = characterResponse.info.next else { return }
+                self.prevPage = prevPage
+                print(characterResponse.info)
                 self.charactersList = characterResponse.results
                 DispatchQueue.main.async {
                     self.charactersTableView.reloadData()
                 }
             } catch {
-                print("Decode Error")
+                print("Decode Error.")
             }
             
         }.resume()
